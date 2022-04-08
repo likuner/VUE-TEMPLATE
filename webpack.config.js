2d8026291f5ced01048d5e340da62da1186e3094
likun <273676636@qq.com>
2022-04-08 17:49:13 +0800

    config babel

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,19 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-runtime']
+            ]
+          }
+        }
+      },
+      {
         test: /\.vue$/,
         use: 'vue-loader'
       },
